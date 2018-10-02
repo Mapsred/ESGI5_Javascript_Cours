51f54a4bf2604b5384fe969e814f1244b886555c
François MATHIEU <francois.mathieu@livexp.fr>
2018-10-02 20:27:24 +0200

    Add mario-kart

---
+++ exercice-4/mario-kart.js
@@ -0,0 +1,36 @@
+function Person() {
+    this.name = name;
+    let state = "normal";
+    let origin = "";
+
+    this.receiveData = (data) => {
+        state = data.state;
+        origin = data.origin;
+    };
+
+    this.needUpdate = () => {
+        return true;
+    };
+
+    const getState = () => {
+        return state;
+    };
+
+    this.speak = () => {
+        switch (state) {
+            case 'ready':
+                console.log('Here we go!');
+                break;
+            case 'happy':
+                console.log("let's have some fun");
+                break;
+            case 'sad':
+                console.log(`Outch!!! Damn ${origin}`);
+                break;
+            default:
+                console.log("");
+                break;
+        }
+    }
+
+}
